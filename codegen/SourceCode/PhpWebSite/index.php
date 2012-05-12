d33bdc14537370bcc30ef6249670b055abe8f355
ajlopez <webmaster@ajlopez.com>
2012-05-12 11:01:35 -0300

    index.php removed from SourceCode initial seed

--- codegen/SourceCode/PhpWebSite/index.php
+++
@@ -1,5 +0,0 @@
-<?
-	$Page->Title = 'Main';
-	include_once('includes/Header.inc.php');
-	include_once('includes/Footer.inc.php');
-?>
--- codegen/SourceCode/PhpWebSiteEs/index.php
+++
@@ -1,5 +0,0 @@
-<?
-	$Page->Title = 'Principal';
-	include_once('includes/Header.inc.php');
-	include_once('includes/Footer.inc.php');
-?>
